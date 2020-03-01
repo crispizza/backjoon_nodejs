81bab243a21f157bca006216fa6f0048bce8f636
apple <apple@appleui-iMac.local>
2020-03-01 18:27:39 +0900

    다 햇슨

--- hotel.js
+++ hotel.js
@@ -1,24 +1,20 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input.push(line);
-    let rep = input[0];
-    let data = input.slice(1);
-    if(input.length>input[0]){
-        for(let i = 0; i < rep ; i++){
-            const currentData = data[i];
-            let split = currentData.split(' ');
-            let h = split[0];
-            let w = split[1];
-            let n = split[2];
-            console.log(h,w,n);
-        }
-        rl.close();
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let rep = input[0];
+let data = input.slice(1);
+for(let r = 0; r < rep ; r++){
+    const currentData = data[r];
+    let split = currentData.split(' ');
+    let h = split[0];
+    let n = split[2];
+    let h_result = 0;
+    let w_result = 0;
+    while(n>0){
+        w_result++;
+        n -= h;
     }
-});
+    h_result = h - Math.abs(n);
+    if(w_result<10){
+        w_result = "0"+w_result.toString();
+    }
+    console.log(parseInt(h_result.toString()+w_result));
+}
